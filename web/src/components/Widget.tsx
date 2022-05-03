4cfb990c0d8f72b10b8f8792a170f3e7980e8fac
Maciel Suassuna <macielsuassuna14@gmail.com>
2022-05-02 21:56:44 -0300

    Final da primeira aula

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+import { ChatTeardropDots } from "phosphor-react"
+import { Popover } from "@headlessui/react"
+
+export const Widget = () => {
+
+    return (
+        <Popover className="absolute bottom-10 right-10">
+            <Popover.Panel>
+                <p>Hello, World!</p>
+            </Popover.Panel>
+
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6" />
+                
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    Feedback
+                </span>
+            </Popover.Button>
+        </Popover>
+    )
+}
